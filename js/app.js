8b9c49ffacc3321ca02bc8b655c94a60c88c6a26
Sebastian <tsebastian@paypal.com>
2014-03-19 14:38:02 +0800

    updated social

--- js/app.js
+++ js/app.js
@@ -137,5 +137,35 @@ $(window).load(function() {
 	$('#down-arrow-container').fadeIn(900).animate({bottom: "20%",}, 100, function() {});
 	preLoaded = true;
 
+
+	// Hide all the tooltips
+	$("#social li").each(function() {
+		$("a strong", this).css("opacity", "0");
+	});
 	
+	$("#social li").hover(function() { // Mouse over
+		$(this)
+			.stop().fadeTo(500, 1)
+			.siblings().stop().fadeTo(500, 0.2);
+			
+		$("a strong", this)
+			.stop()
+			.animate({
+				opacity: 1,
+				top: "-10px"
+			}, 300);
+		
+	}, function() { // Mouse out
+		$(this)
+			.stop().fadeTo(500, 1)
+			.siblings().stop().fadeTo(500, 1);
+			
+		$("a strong", this)
+			.stop()
+			.animate({
+				opacity: 0,
+				top: "-1px"
+			}, 300);
+	});
+
 });
